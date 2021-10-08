2c5f07cbbae6ad0da29ea8f422c5ceb3b152828f
Alexander Feiertag <alexfeiertag@gmail.com>
2021-10-08 21:20:12 +0200

    [134] MessagingRework

--- s4s-frontend/src/service/backend/registration.service.ts
+++ s4s-frontend/src/service/backend/registration.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {BackendClientService} from "./backendClientService";
+import {BackendClientService} from "../backendClientService";
 
 @Injectable({providedIn: 'root'})
 export class RegistrationService extends BackendClientService {
