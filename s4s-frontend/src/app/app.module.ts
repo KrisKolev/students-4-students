384dfd84d4509b142dd462fdc4480d2368a007bc
Alexander Feiertag <40577231+AlexanderFeiertag@users.noreply.github.com>
2021-10-18 13:16:59 +0200

    Update app.module.ts
    code formatting

--- s4s-frontend/src/app/app.module.ts
+++ s4s-frontend/src/app/app.module.ts
@@ -1,20 +1,20 @@
-import {BrowserModule} from '@angular/platform-browser';//
-import {NgModule} from '@angular/core';//
-import {AppRoutingModule} from './app-routing.module';//
-import {AppComponent} from './app.component';//
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';//
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
-import {FirebaseService} from '../service/firebase.service';//
+import {FirebaseService} from '../service/firebase.service';
 import {initializeApp} from 'firebase/app';//
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';//
-import {MatFormFieldModule} from '@angular/material/form-field';//
-import {MatInputModule} from '@angular/material/input';//
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';//
-import {MatIconModule} from '@angular/material/icon';//
-import {MatDialogModule} from '@angular/material/dialog';//
-import {LoginDialogComponent} from './login-dialog/login-dialog.component';//
-import {MatButtonModule} from '@angular/material/button';//
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
+import {LoginDialogComponent} from './login-dialog/login-dialog.component';
+import {MatButtonModule} from '@angular/material/button';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatMenuModule} from '@angular/material/menu';
 
