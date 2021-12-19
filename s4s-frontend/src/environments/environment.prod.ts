cd90517c330613ec871b6ebefe1b14bed79ec5c2
madmike270 <mifa2022@outlook.com>
2021-12-19 13:16:50 +0100

    adapted frontend environment

--- s4s-frontend/src/environments/environment.prod.ts
+++ s4s-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
     production: true,
-    baseUrl: '../api/',
+    //use for local development
+    baseUrl: 'http://localhost:8080/api/',
+
+    //use for azure deployment
+    //baseUrl: 'https://spring-boot-complete-1639580628117.azurewebsites.net/api/',
     geoLocationServiceUrl: 'https://api.bigdatacloud.net/data/reverse-geocode-client'
 };
--- s4s-frontend/src/environments/environment.ts
+++ s4s-frontend/src/environments/environment.ts
@@ -4,7 +4,11 @@
 
 export const environment = {
     production: false,
-    baseUrl: 'http://localhost:8080/s4s-backend/api/',
+    //use for local development
+    baseUrl: 'http://localhost:8080/api/',
+
+    //use for azure deployment
+    //baseUrl: 'https://spring-boot-complete-1639580628117.azurewebsites.net/api/',
     geoLocationServiceUrl: 'https://api.bigdatacloud.net/data/reverse-geocode-client'
 };
 
